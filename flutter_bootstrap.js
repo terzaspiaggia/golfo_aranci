b5c2b6bef9bcd0dd38539c6c4365e2efd5abcf28
terzaspiaggia <terzaspiaggia.ordinazioni@gmail.com>
2024-05-28 20:36:25 +0200

    added splash screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2191766885"
+    serviceWorkerVersion: "103757728"
   }
 });
